aab278899300e3170afc6f80ad353113c571e533
xTalAlex <talale93@hotmail.it>
2022-06-10 07:47:43 +0200

    Migrations Down

--- database/migrations/2022_06_03_235020_create_webhook_calls_table.php
+++ database/migrations/2022_06_03_235020_create_webhook_calls_table.php
@@ -19,4 +19,14 @@ return new class extends Migration
             $table->timestamps();
         });
     }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webhook_calls');
+    }
 };
