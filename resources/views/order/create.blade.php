efd241e0e7e2961f3e55d1e4a0e453f3d683751f
xTalAlex <talale93@hotmail.it>
2022-06-21 16:50:10 +0200

    CET

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -119,7 +119,7 @@
                             <input id="same_address" name="same_address" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                                 wire:model="same_address"  value=""
                             >
-                            <label for="same_address" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Use as billing address</label>
+                            <label for="same_address" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ __('Use as billing address') }}</label>
                         </div>
 
                         @auth
