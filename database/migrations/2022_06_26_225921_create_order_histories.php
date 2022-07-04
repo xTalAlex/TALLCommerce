fbcbb69523ebd90507069209e059a26882a3538a
xTalAlex <talale93@hotmail.it>
2022-07-04 08:26:55 +0200

    Update 2022_06_26_225921_create_order_histories.php

--- database/migrations/2022_06_26_225921_create_order_histories.php
+++ database/migrations/2022_06_26_225921_create_order_histories.php
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders_histories');
+        Schema::dropIfExists('order_histories');
     }
 };
