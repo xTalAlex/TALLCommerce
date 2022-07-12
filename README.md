3e9028c1ced59e641dc7c6d26310fab4d9b8efce
xTalAlex <talale93@hotmail.it>
2022-07-12 03:00:40 +0200

    laravel debugbar

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ cron per pulizia payment_failed
 utilizzo di un codice coupon alla volta
 
 shipping price fissi
+
+prodotti con varianti > vengono mostrati prezzo, quantità e foto della variante principale
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Illuminate\Database\Eloquent\Model::preventLazyLoading(! app()->isProduction());
     }
 }
