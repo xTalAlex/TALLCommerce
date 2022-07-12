07cc134ea65f5cb93215ec86bb75763b2ce93ef6
xTalAlex <talale93@hotmail.it>
2022-07-12 04:19:39 +0200

    vite hotupdate

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,17 @@ export default defineConfig({
             'resources/css/app.css',
             'resources/js/app.js',
         ]),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if(file.endsWith('.blade.php')){
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',              
+                    });
+                }
+            }
+        },
     ],
 
 });
