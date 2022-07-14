45c6719b946c55c7e75adf93eaede2eecc42ffa7
xTalAlex <talale93@hotmail.it>
2022-07-14 05:17:06 +0200

    keen-slider carousel

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,41 +1,84 @@
-<div id="default-carousel" class="relative" data-carousel="static">
-
-    <div class="relative h-80 overflow-hidden rounded-lg sm:h-96 xl:h-[28rem] 2xl:h-[32rem]">
-        @forelse($products as $product)
-            <div class="absolute inset-0 z-20 transition-all duration-700 ease-in-out transform translate-x-0" data-carousel-item="">
-                <span class="absolute z-30 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">
-                    {{ $product->name }}
-                </span>
-                <img src="{{ $product->image }}" class="absolute block object-cover w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="{{$product->name}}">
+<div  
+    x-data="{
+        slider : null,
+        nSlides : {{ $products->count() }},
+        currentIdx : 1,
+        timeout : null,
+        mouseOver : false,
+        visible : false,
+    }"
+    x-init="
+        slider = new window.KeenSlider(
+            '#slider',
+            {
+                loop: true,
+                created: () => {
+                    visible = true;
+                },
+            },
+            [
+                slider => {
+                    function clearNextTimeout() {
+                        clearTimeout(timeout)
+                    }
+                    function nextTimeout() {
+                        clearTimeout(timeout)
+                        if (!mouseOver)
+                            timeout = setTimeout(() => {
+                                slider.next()
+                            }, 3000)
+                    }
+                    slider.on('created', () => {
+                        slider.container.addEventListener('mouseover', () => {
+                            mouseOver = true
+                            clearNextTimeout()
+                        })
+                        slider.container.addEventListener('mouseout', () => {
+                            mouseOver = false
+                            nextTimeout()
+                        })
+                        nextTimeout()
+                    })
+                    slider.on('dragStarted', clearNextTimeout)
+                    slider.on('animationEnded', nextTimeout)
+                    slider.on('updated', nextTimeout)
+                    slider.on('slideChanged', () => {
+                        currentIdx = slider.track.details.abs % nSlides + 1;
+                    })
+                },
+            ]
+        )
+    "
+>
+    <div class="relative">
+        <div class="keen-slider h-96 xl:h-[28rem] 2xl:h-[32rem]"
+            id="slider"
+            x-show = "visible"
+            x-cloak
+        >
+            @foreach($products as $product)
+            <div class="relative w-full h-full bg-white keen-slider__slide">
+                <img class="object-cover w-full h-full" src="{{$product->image}}"/>
+                <span class="absolute z-30 flex items-center justify-center w-full h-20 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 top-1/2 left-1/2 sm:text-3xl">
+                    {{ $product->name }} 
+                </span>    
             </div>
-        @empty
-            <div class="absolute inset-0 z-20 transition-all duration-700 ease-in-out transform translate-x-0" data-carousel-item="">
-                <span class="absolute z-30 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">
-                    
-                </span>
-                <img src="" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="">
-            </div>
-        @endforelse
+            @endforeach
+        </div>
+        <div x-on:click="slider.prev()" class="absolute w-8 h-8 -translate-y-1/2 cursor-pointer left-1 fill-white top-1/2">
+            <x-icons.chevron-left></x-icons.chevron-left>
+        </div>
+        <div x-on:click="slider.next()" class="absolute w-8 h-8 -translate-y-1/2 cursor-pointer right-1 fill-white top-1/2">
+            <x-icons.chevron-right></x-icons.chevron-right>
+        </div>
+        <div class="flex justify-center py-2">
+            @foreach($products as $product)
+                <div class="w-3 h-3 p-1 mx-1 border-none rounded-full cursor-pointer"
+                    x-bind:class="currentIdx == {{ $loop->iteration }} ? 'bg-black' : 'bg-gray-400 '"
+                    x-on:click="slider.moveToIdx({{ $loop->iteration }})" 
+                ></div>
+            @endforeach
+        </div>
     </div>
 
-    <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        @foreach($products as $product)
-            <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800" aria-current="{{ $loop->first ? 'true' : 'false'}}" aria-label="Slide {{ $loop->iteration }}" data-carousel-slide-to="{{ $loop->index }}"></button>
-        @endforeach
-    </div>
-
-    <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev="">
-        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-            <span class="hidden">{{ trans_choice('general.previous',1) }}</span>
-        </span>
-    </button>
-
-    <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next="">
-        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-            <span class="hidden">{{ trans_choice('general.next',1) }}</span>
-        </span>
-    </button>
-    
 </div>
