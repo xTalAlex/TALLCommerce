d93d051d960bdaaa85ce2f1f2456bf12fc055a79
xTalAlex <talale93@hotmail.it>
2022-07-14 18:40:57 +0200

    laravel-invoices install

---
+++ resources/lang/vendor/invoices/it/invoice.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'invoice'                => 'Invoice',
+    'serial'                 => 'N.',
+    'date'                   => 'Invoice date',
+    'seller'                 => 'Seller',
+    'buyer'                  => 'Buyer',
+    'address'                => 'Indirizzo',
+    'code'                   => 'Code',
+    'vat'                    => 'P.IVA',
+    'phone'                  => 'Telefono',
+    'description'            => 'Descrizione',
+    'units'                  => 'Unità',
+    'quantity'               => 'Quantità',
+    'price'                  => 'Prezzo',
+    'discount'               => 'Sconto',
+    'tax'                    => 'IVA',
+    'sub_total'              => 'Subtotale',
+    'total_discount'         => 'Totale sconto',
+    'taxable_amount'         => 'Taxable amount',
+    'total_taxes'            => 'Totale IVA',
+    'tax_rate'               => 'Tax rate',
+    'total_amount'           => 'Totale',
+    'pay_until'              => 'Please pay until',
+    'amount_in_words'        => 'Amount in words',
+    'amount_in_words_format' => '%s %s e %s %s',
+    'notes'                  => 'Note',
+    'shipping'               => 'Spedizione',
+    'paid'                   => 'Pagato',
+    'due'                    => 'Due',
+];
