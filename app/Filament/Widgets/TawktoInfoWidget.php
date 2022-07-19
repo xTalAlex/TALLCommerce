0e1aeca2764873c69d9743978453dfbc726b704d
xTalAlex <talale93@hotmail.it>
2022-07-19 08:33:31 +0200

    TawktoInfoWidget

---
+++ app/Filament/Widgets/TawktoInfoWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class TawktoInfoWidget extends Widget
+{
+    protected static string $view = 'filament.widgets.tawkto-info-widget';
+}
---
+++ resources/views/filament/widgets/tawkto-info-widget.blade.php
@@ -0,0 +1,50 @@
+<x-filament::widget class="filament-filament-info-widget">
+    <x-filament::card class="relative">
+        
+        <div class="relative flex flex-col items-center justify-center h-12 space-y-2">
+            <div class="space-y-1">
+                <a
+                    href="https://www.tawk.to/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img class="h-6" src="img/logos/tawkto-noicon.png" />
+                </a>
+            </div>
+
+            <div class="flex space-x-2 text-sm rtl:space-x-reverse">
+                <a
+                    href="https://dashboard.tawk.to/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    @class([
+                        'text-gray-600 hover:text-primary-500 focus:outline-none focus:underline',
+                        'dark:text-gray-300 dark:hover:text-primary-500' => config('filament.dark_mode'),
+                    ])
+                >
+                    Dashboard
+                </a>
+
+                <span>
+                    &bull;
+                </span>
+
+                <a
+                    href="https://talale.tawk.help"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    @class([
+                        'text-gray-600 hover:text-primary-500 focus:outline-none focus:underline',
+                        'dark:text-gray-300 dark:hover:text-primary-500' => config('filament.dark_mode'),
+                    ])
+                >
+                    Knowledge Base
+                </a>
+            </div>
+
+        </div>
+            
+        <img class="absolute bottom-0 left-0 h-12" style="bottom : -0.25rem;" src="img/logos/tawkto-icon.png"/>
+            
+    </x-filament::card>
+</x-filament::widget>
